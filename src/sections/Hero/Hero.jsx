17abdf332813a91be1551cc1b7cc68bf181083f8
fynnbl <fblaurock@phoenixcontact.com>
2024-07-10 11:42:39 +0200

    Added different Color Modes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,14 +1,23 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
-import themeIcon from "../../assets/sun.svg";
-import linkedinIcon from "../../assets/linkedin-light.svg";
-import githubIcon from "../../assets/github-light.svg";
-import emailIcon from "../../assets/email.svg";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import emailLight from "../../assets/email-light.svg";
+import emailDark from "../../assets/email-dark.svg";
 import CV from "../../assets/cv_fbl.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const emailIcon = theme === "light" ? emailLight : emailDark;
 
   return (
     <section id='hero' className={styles.container}>
