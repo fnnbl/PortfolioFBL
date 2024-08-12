183b05555e8061234d2cd96aeb5c4003a0e7f9a0
fynnbl <blaurock.fynn@gmail.com>
2024-08-12 10:08:54 +0200

    Added Contact Form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
     <>
       <Hero />
       <Projects />
+      <Contact />
     </>
   );
 }
